b58d2f3aad636a06954d634aa3f52ab96293de70
Daniel Gordon <Dgordon8765@gmail.com>
2017-04-21 09:22:26 -0500

    Added Vim, VimAirline, and Pathogen installs scripts

---
+++ installVimAirline.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
